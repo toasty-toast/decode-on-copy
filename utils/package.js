adfe0c2f7299f2a0644df8e2e1a4440177b1e9c6
Brian McDonald <bmcdonald916@gmail.com>
2019-07-29 12:10:06 -0400

    Add support for packaging for Chrome

---
+++ utils/package.js
@@ -0,0 +1,24 @@
+const AdmZip = require('adm-zip');
+const fs = require('fs');
+const path = require('path');
+
+const distPath = path.resolve(__dirname, '..', 'dist');
+
+module.exports = function package(extension) {
+	if (!fs.existsSync(distPath)) {
+		console.log(`Could not find directory: \"${distPath}\"`);
+		process.exit(1);
+	}
+
+	const zip = new AdmZip();
+	fs.readdirSync(distPath).forEach(item => {
+		const fullPath = path.resolve(distPath, item);
+		const stat = fs.statSync(fullPath);
+		if (stat && stat.isDirectory()) {
+			zip.addLocalFolder(fullPath, item);
+		} else {
+			zip.addLocalFile(fullPath);
+		}
+	});
+	zip.writeZip(path.resolve(__dirname, '..', `decode-on-copy.${extension}`));
+}
